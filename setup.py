bc43eea17b00645d8575e71989bcdfec7f74f451
Andreas Rath <andreas.rath@ondewo.com>
2025-03-24 10:57:40 +0100

    Added version update in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from typing import List
 
 import setuptools
 
+from ondewo.version import __version__
+
 
 def read_file(file_path: str, encoding: str = 'utf-8') -> str:
     with open(file_path, 'r', encoding=encoding) as f:
@@ -24,7 +26,7 @@ requires: List[str] = read_requirements('requirements.txt')
 
 setuptools.setup(
     name='ondewo-client-utils',
-    version='2.0.0',
+    version=f'{__version__}',
     author='Ondewo GmbH',
     author_email='office@ondewo.com',
     description='This library contains utilities and base classes for gRPC clients.',
@@ -39,12 +41,11 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=requires,
 )
