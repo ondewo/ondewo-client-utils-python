e2e57ebc1a3bb1a9a49bb1f6c033c83a6b128781
Andreas Rath <andreas.rath@ondewo.com>
2026-07-06 19:25:19 +0200

    [OND211-2418] chore: update copyright end year to 2026 in all file headers
    Bump the copyright end year to 2026 across all source and test file headers
    (2017-2024 -> 2017-2026, 2020-2024 -> 2020-2026, and the single-year 2021 -> 2021-2026).

--- tests/test_base_client.py
+++ tests/test_base_client.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2024 ONDEWO GmbH
+# Copyright 2017-2026 ONDEWO GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
